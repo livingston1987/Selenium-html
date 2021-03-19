faa1f5fc7e30f5358bcd7012b55b132bb58a9550
Git Server Admin <admin@git-server>
2021-03-19 01:25:45 +0000

    Add initial repository

---
+++ src/main/java/com/hackerrank/selenium/OnlineSurvey.java
@@ -0,0 +1,14 @@
+package com.hackerrank.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class OnlineSurvey {
+
+    public static void fillDateOfBirth(WebDriver driver, String pageUrl) {
+        // complete implementation
+    }
+
+    public static void answerQuestions(WebDriver driver, String pageUrl) {
+        // complete implementation
+    }
+}
